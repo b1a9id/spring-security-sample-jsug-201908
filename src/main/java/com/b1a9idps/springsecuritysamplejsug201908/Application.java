fe625ff6c386dc54d9ef755a6a2be4979fcdbb02
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2019-07-21 20:42:39 +0900

    プロジェクト作成

---
+++ src/main/java/com/b1a9idps/springsecuritysamplejsug201908/Application.java
@@ -0,0 +1,13 @@
+package com.b1a9idps.springsecuritysamplejsug201908;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
